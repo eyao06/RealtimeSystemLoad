379e225ba2f32f387d4633b6748f2f7a54c65e53
eyao06 <eyao06@yahoo.com>
2017-10-23 20:42:31 -0700

    cleaned up code

--- server/app.js
+++ server/app.js
@@ -1,9 +1,7 @@
     const express = require("express"); 
     const app = express();
-
     const http = require("http");
     const socketIo = require("socket.io");
-
     const port = process.env.PORT || 4001;
     const index = require("./routes/index");
     
